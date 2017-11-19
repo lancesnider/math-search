de47f9ef98957d91c1791aa60a78caf102eda551
Lance <lance@lancesnider.com>
2017-11-19 12:18:51 +1100

    Basic layout

--- App.js
+++ App.js
@@ -1,13 +1,64 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <View>
+          <Text style={styles.gameInfo}>Time: 3:00</Text>
+          <Text style={styles.gameInfo}>High Score: 100</Text>
+          <Text style={styles.gameInfo}>Score: 23</Text>
+        </View>
+        <View style={styles.operandsHolder}>
+          <View style={styles.operand}>
+            <Button color='#272822' title='1' />
+          </View>
+          <View style={styles.operand}>
+            <Button color='#272822' title='2' />
+          </View>
+          <View style={styles.operand}>
+            <Button color='#272822' title='3' />
+          </View>
+          <View style={styles.operand}>
+            <Button color='#272822' title='4' />
+          </View>
+          <View style={styles.operand}>
+            <Button color='#272822' title='5' />
+          </View>
+          <View style={styles.operand}>
+            <Button color='#272822' title='1' />
+          </View>
+          <View style={styles.operand}>
+            <Button color='#272822' title='2' />
+          </View>
+          <View style={styles.operand}>
+            <Button color='#272822' title='3' />
+          </View>
+          <View style={styles.operand}>
+            <Button color='#272822' title='4' />
+          </View>
+          <View style={styles.operand}>
+            <Button color='#272822' title='5' />
+          </View>
+        </View>
+        <View style={styles.operatorsHolder}>
+          <View style={styles.operator}>
+            <Button color='white' title='+' />
+          </View>
+          <View style={styles.operator}>
+            <Button color='white' title='-' />
+          </View>
+          <View style={styles.operator}>
+            <Button color='white' title='*' />
+          </View>
+          <View style={styles.operator}>
+            <Button color='white' title='/' />
+          </View>
+          <View style={styles.operator}>
+            <Button color='white' title='^' />
+          </View>
+        </View>
       </View>
     );
   }
@@ -16,8 +67,32 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#272822',
+    padding: 35
   },
+  gameInfo: {
+    color: 'white'
+  },
+  operandsHolder: {
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  },
+  operand: {
+    width: '20%',
+    height: 60,
+    backgroundColor: '#e0e0e0',
+    borderWidth: 0.5,
+    borderColor: '#272822'
+  },
+  operatorsHolder: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  operator: {
+    width: '20%',
+    height: 60,
+    backgroundColor: '#f5923e',
+    borderWidth: 0.5,
+    borderColor: '#272822'
+  }
 });
--- src/utils/random-tile-numbers.js
+++
@@ -1,9 +0,0 @@
-const randomTileNumbers = () => {
-  var randomNumbersArray = []
-  for (var i = 0; i < 25; i++) {
-    randomNumbersArray[i] = Math.floor(Math.random() * 10)
-  }
-  return randomNumbersArray
-}
-
-export default randomTileNumbers
