f52cbdf45699db35ec1d854c2960afd0a78b4512
Lance <lance@lancesnider.com>
2017-12-17 12:16:14 -0800

    Add acioctions and reducers

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { StyleSheet, View } from 'react-native'
-
-import GameInfo from './src/GameInfo'
-import Operands from './src/Operands'
-import Operators from './src/Operators'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import gameDataReducer from './src/reducers/reducer_game_data'
+import GameInfo from './src/components/GameInfo'
+import Operands from './src/components/Operands'
+import Operators from './src/components/Operators'
 
 const styles = StyleSheet.create({
   container: {
@@ -14,14 +16,18 @@ const styles = StyleSheet.create({
   }
 })
 
+const store = createStore(gameDataReducer)
+
 class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <GameInfo />
-        <Operands tileNumbers={[1,2,3,6,4,1,2,2,3,5,5,6,6,7,9,5,4,7,3,2,5,7,8,9,0]} />
-        <Operators />
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <GameInfo />
+          <Operands tileNumbers={[1,2,3,6,4,1,2,2,3,5,5,6,6,7,9,5,4,7,3,2,5,7,8,9,0]} />
+          <Operators />
+        </View>
+      </Provider>
     )
   }
 }
---
+++ src/actions/index.js
@@ -0,0 +1,36 @@
+export const clickPlay = () => {
+  return {
+    type: 'CLICK_PLAY',
+    payload: true
+  }
+}
+
+export const gameOver = () => {
+  return {
+    type: 'GAME_OVER',
+    payload: false
+  }
+}
+
+export const clickOperator = (operatorClicked) => {
+  return {
+    type: 'CLICK_CALCULATOR',
+    itemClicked: operatorClicked,
+    tileClicked: operatorClicked
+  }
+}
+
+export const clickOperand = (operandClicked, tileClicked) => {
+  return {
+    type: 'CLICK_CALCULATOR',
+    itemClicked: operandClicked,
+    tileClicked: tileClicked
+  }
+}
+
+export const updateFeedback = (newFeedback) => {
+  return {
+    type: 'NEW_FEEDBACK',
+    newFeedback: newFeedback
+  }
+}
